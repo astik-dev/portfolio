93409cf63422632baffad737ba32ad76f74f16de
ASTIK <astik.ytt@gmail.com>
2023-11-06 19:13:23 +0100

    Refactor: Replaced fetch() request with a new fetchJSON()

--- js/script.js
+++ js/script.js
@@ -272,20 +272,21 @@ function removeEmptyItems(className, itemsContainer) {
 }
 
 
-let projects;
-fetch('projects.json')
-	.then(response => response.json())
-	.then(data => {
-		projects = data;
+const fetchJSON = url => fetch(url).then(response => response.json());
 
-		let amt = 4;
-		generateProjects(amt, "start");
-		if (projects.length > amt) {
-			dqs(".projects__container")
-				.insertAdjacentHTML("beforeend", 
-									`<button class="projects__btn-load-more" onClick="generateProjects(4, 'load more')">Load More</button>`);
-		}
-	});
+
+let projects;
+fetchJSON('projects.json').then(data => {
+	projects = data;
+
+	let amt = 4;
+	generateProjects(amt, "start");
+	if (projects.length > amt) {
+		dqs(".projects__container")
+			.insertAdjacentHTML("beforeend", 
+								`<button class="projects__btn-load-more" onClick="generateProjects(4, 'load more')">Load More</button>`);
+	}
+});
 
 const projectsItems = dqs(".projects__items");
 
@@ -363,12 +364,10 @@ function generateProjects(amt, mode) {
 
 
 let skills;
-fetch('skills.json')
-	.then(response => response.json())
-	.then(data => {
-		skills = data;
-		generateSkills();
-	});
+fetchJSON('skills.json').then(data => {
+	skills = data;
+	generateSkills();
+});
 
 function generateSkills() {
 	const skillsItems = dqs(".skills__items");
@@ -401,102 +400,98 @@ function generateSkills() {
 
 
 let reviews;
-fetch('reviews.json')
-	.then(response => response.json())
-	.then(data => {
+fetchJSON('reviews.json').then(data => {
 
-		reviews = data;
+	reviews = data;
 
-		reviews.sort(function (a, b) {
-			return -(a.text.length - b.text.length);
-		});
-		
-
-
-		const reviewsSwiperWrapper = dqs(".reviews__swiper-wrapper");
-
-		removeEmptyItems(".reviews__slide_empty", reviewsSwiperWrapper);
-
-		reviews.forEach((review) => {
-
-			let reviewAvatar;
-			if (review.avatar == "") {
-				reviewAvatar = `<img src="img/reviews/user-avatar.svg" alt="Avatar">`;
-			} else {
-				reviewAvatar = `<picture>
-									<source type="image/webp" srcset="img/reviews/avatar/${review.avatar}.webp">
-									<source type="image/jpeg" srcset="img/reviews/avatar/${review.avatar}.jpg">
-									<img src="img/reviews/avatar/${review.avatar}.jpg" alt="Avatar">
-								</picture>`;
-			}
-
-			gradeColor = "#2CB67D";
-			if (Number(review.grade) < 5) {gradeColor = "#fa1111"}
-			else if (Number(review.grade) < 7) {gradeColor = "#fab43c"}
-
-			let currentReview =`<div class="reviews__slide swiper-slide">
-									<div class="reviews__slide-top">
-										${reviewAvatar}
-										<h5>${review.name}</h5>
-										<h4 class="reviews__slide-grade">
-											<span style="color: ${gradeColor};">${review.grade}/10</span>
-											<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"viewBox="0 0 47.94 47.94" xml:space="preserve"> <path fill="${gradeColor}" d="M26.285,2.486l5.407,10.956c0.376,0.762,1.103,1.29,1.944,1.412l12.091,1.757 c2.118,0.308,2.963,2.91,1.431,4.403l-8.749,8.528c-0.608,0.593-0.886,1.448-0.742,2.285l2.065,12.042 c0.362,2.109-1.852,3.717-3.746,2.722l-10.814-5.685c-0.752-0.395-1.651-0.395-2.403,0l-10.814,5.685 c-1.894,0.996-4.108-0.613-3.746-2.722l2.065-12.042c0.144-0.837-0.134-1.692-0.742-2.285l-8.749-8.528 c-1.532-1.494-0.687-4.096,1.431-4.403l12.091-1.757c0.841-0.122,1.568-0.65,1.944-1.412l5.407-10.956 C22.602,0.567,25.338,0.567,26.285,2.486z"/> </svg>
-										</h4>
-									</div>
-									<p>${review.text}</p>
-									<div class="reviews__slide-bottom">
-										<span>${review.date}</span>
-										<a href="${review.link}" target="_blank" title="Go to source">${review.linkText}</a>
-									</div>
-								</div>`
-
-			reviewsSwiperWrapper.insertAdjacentHTML("beforeend", currentReview);
-		});
-
-
-
-		const swiperReviews = new Swiper('.reviews__swiper', {
-
-			pagination: {
-				el: '.reviews__swiper-pagination',
-				type: 'fraction',
-			},
-
-			navigation: {
-				nextEl: '.reviews__swiper-nav_right',
-				prevEl: '.reviews__swiper-nav_left',
-			},
-
-			autoHeight: true,
-			grabCursor: true,
-		
-			breakpoints: {	
-				// when window width is >= 767.5px
-				767.5: {
-					resistanceRatio: 0,
-				},
-			},
-
-			effect: "flip",
-			speed: 450,
-
-			on: {
-				slideChange: function () {
-					gtmEvent({'event': 'reviews_slideChange'});
-				},
-			},
-		});
+	reviews.sort(function (a, b) {
+		return -(a.text.length - b.text.length);
 	});
+	
+
+
+	const reviewsSwiperWrapper = dqs(".reviews__swiper-wrapper");
+
+	removeEmptyItems(".reviews__slide_empty", reviewsSwiperWrapper);
+
+	reviews.forEach((review) => {
+
+		let reviewAvatar;
+		if (review.avatar == "") {
+			reviewAvatar = `<img src="img/reviews/user-avatar.svg" alt="Avatar">`;
+		} else {
+			reviewAvatar = `<picture>
+								<source type="image/webp" srcset="img/reviews/avatar/${review.avatar}.webp">
+								<source type="image/jpeg" srcset="img/reviews/avatar/${review.avatar}.jpg">
+								<img src="img/reviews/avatar/${review.avatar}.jpg" alt="Avatar">
+							</picture>`;
+		}
+
+		gradeColor = "#2CB67D";
+		if (Number(review.grade) < 5) {gradeColor = "#fa1111"}
+		else if (Number(review.grade) < 7) {gradeColor = "#fab43c"}
+
+		let currentReview =`<div class="reviews__slide swiper-slide">
+								<div class="reviews__slide-top">
+									${reviewAvatar}
+									<h5>${review.name}</h5>
+									<h4 class="reviews__slide-grade">
+										<span style="color: ${gradeColor};">${review.grade}/10</span>
+										<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"viewBox="0 0 47.94 47.94" xml:space="preserve"> <path fill="${gradeColor}" d="M26.285,2.486l5.407,10.956c0.376,0.762,1.103,1.29,1.944,1.412l12.091,1.757 c2.118,0.308,2.963,2.91,1.431,4.403l-8.749,8.528c-0.608,0.593-0.886,1.448-0.742,2.285l2.065,12.042 c0.362,2.109-1.852,3.717-3.746,2.722l-10.814-5.685c-0.752-0.395-1.651-0.395-2.403,0l-10.814,5.685 c-1.894,0.996-4.108-0.613-3.746-2.722l2.065-12.042c0.144-0.837-0.134-1.692-0.742-2.285l-8.749-8.528 c-1.532-1.494-0.687-4.096,1.431-4.403l12.091-1.757c0.841-0.122,1.568-0.65,1.944-1.412l5.407-10.956 C22.602,0.567,25.338,0.567,26.285,2.486z"/> </svg>
+									</h4>
+								</div>
+								<p>${review.text}</p>
+								<div class="reviews__slide-bottom">
+									<span>${review.date}</span>
+									<a href="${review.link}" target="_blank" title="Go to source">${review.linkText}</a>
+								</div>
+							</div>`
+
+		reviewsSwiperWrapper.insertAdjacentHTML("beforeend", currentReview);
+	});
+
+
+
+	const swiperReviews = new Swiper('.reviews__swiper', {
+
+		pagination: {
+			el: '.reviews__swiper-pagination',
+			type: 'fraction',
+		},
+
+		navigation: {
+			nextEl: '.reviews__swiper-nav_right',
+			prevEl: '.reviews__swiper-nav_left',
+		},
+
+		autoHeight: true,
+		grabCursor: true,
+	
+		breakpoints: {	
+			// when window width is >= 767.5px
+			767.5: {
+				resistanceRatio: 0,
+			},
+		},
+
+		effect: "flip",
+		speed: 450,
+
+		on: {
+			slideChange: function () {
+				gtmEvent({'event': 'reviews_slideChange'});
+			},
+		},
+	});
+});
 
 
 
 let contacts;
-fetch('contacts.json')
-	.then(response => response.json())
-	.then(data => {
-		contacts = data;
-		generateContacts();
-	});
+fetchJSON('contacts.json').then(data => {
+	contacts = data;
+	generateContacts();
+});
 
 function generateContacts() {
 	const contactsItems = dqs(".contacts__items");
