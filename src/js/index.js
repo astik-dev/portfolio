162b174a7c49f51b8cbbae0590d869e8db63df6b
ASTIK <astik.ytt@gmail.com>
2024-04-19 19:47:42 +0200

    Refactor: Decompose imageCreator into separate module

--- src/js/index.js
+++ src/js/index.js
@@ -1,49 +1,9 @@
 import { doc, dqs, dqsa } from "./modules/utils.js";
+import imageCreator from "./modules/imageCreator.js";
 
 
-
-// script.js
 
 let openClosePermissionProjectPopup = true;
-
-
-
-const getFileExtension = filePath => filePath.split('.').pop();
-
-const imageCreator = {
-
-	imgBasePath: {
-		local: "img/",
-		external: "https://raw.githubusercontent.com/astik-dev/portfolio/content/images/",
-	},
-
-	px1: `img/1x1.png`,
-
-	fullPath: function (source, path) {return this.imgBasePath[source] + path},
-
-	newWebpPic: function (source, webpPath, fallbackPath, alt, lazy) {
-		const types = {
-			jpg: "jpeg",
-		}
-
-		const src = lazy ? "data-src" : "srcset";
-		
-		const fallbackExt = getFileExtension(fallbackPath);
-		const fallbackType = types[fallbackExt] || fallbackExt;
-		
-		return `<picture>
-					<source type="image/webp" ${src}="${this.fullPath(source, webpPath)}">
-					<source type="image/${fallbackType}" ${src}="${this.fullPath(source, fallbackPath)}">
-					${this.newImg(source, fallbackPath, alt, lazy)}
-				</picture>`;
-	},
-
-	newImg: function (source, path, alt, lazy) {
-		const fullPath = this.fullPath(source, path);
-		const src = lazy ? `"${this.px1}" data-src="${fullPath}"` : `"${fullPath}"`;
-		return `<img src=${src} alt="${alt}">`;
-	}
-}
 
 
 
