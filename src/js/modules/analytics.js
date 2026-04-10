a4967495c20ef82b23f60ab1264a0b8bd62534a0
ASTIK <52823963+astik-dev@users.noreply.github.com>
2026-04-10 23:50:24 +0200

    feat: queue analytics track calls until umami loads

--- src/js/modules/analytics.js
+++ src/js/modules/analytics.js
@@ -1,1 +1,23 @@
-export const track = (...args) => window.umami?.track(...args);
+/**
+ * @type {any[][] | null}
+ */
+let pendingTrackCalls = [];
+
+export const track = (...args) => {
+	if (window.umami) {
+		window.umami.track(...args);
+	} else if (pendingTrackCalls) {
+		pendingTrackCalls.push(args);
+	}
+};
+
+const umamiScript = document.querySelector("script[src='js/u.js']");
+
+umamiScript.addEventListener("load", () => {
+	if (window.umami) {
+		pendingTrackCalls.forEach(args => window.umami.track(...args));
+	}
+	pendingTrackCalls = null;
+});
+
+umamiScript.addEventListener("error", () => pendingTrackCalls = null);
