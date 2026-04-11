14df51510d0409ca9a83f22099ab95c3e8396e95
ASTIK <52823963+astik-dev@users.noreply.github.com>
2026-04-11 20:39:56 +0200

    refactor: inline eye icon to SVG sprite

--- src/js/modules/renderProjectsItem.js
+++ src/js/modules/renderProjectsItem.js
@@ -12,6 +12,7 @@ export function renderProjectsItem({ folder, title }, index) {
 		<article class="projects__item" data-project-index="${index}">
 			${webpPicElem}
 			<div class="projects__item-title">
+				<svg><use href="#icon-eye" /></svg>
 				<h5>${title}</h5>
 			</div>
 		</article>
