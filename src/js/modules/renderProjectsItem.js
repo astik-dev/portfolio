f9a40053a4990ad32c047dc44350d24188d91776
ASTIK <52823963+astik-dev@users.noreply.github.com>
2026-04-13 01:43:24 +0200

    perf: replace loading.gif with CSS shimmer effect in .projects__item

--- src/js/modules/renderProjectsItem.js
+++ src/js/modules/renderProjectsItem.js
@@ -3,16 +3,27 @@ import imageCreator from "./imageCreator.js";
 export function renderProjectsItem({ folder, title }, index) {
 	
 	const img800 = "projects/" + folder + "/800";
-	const webp = img800 + ".webp";
-	const fallback = img800 + ".jpeg";
-		
-	const webpPicElem = imageCreator.newWebpPic(
-		"external", webp, fallback, title, undefined, "high"
-	);
-	
+	const webp = imageCreator.fullPath("external", img800 + ".webp");
+	const jpeg = imageCreator.fullPath("external", img800 + ".jpeg");
+
+	const handleImageLoad =
+		"this.closest('.projects__item').classList.remove('shimmer')";
+
 	return `
-		<article class="projects__item" data-project-index="${index}">
-			${webpPicElem}
+		<article
+			class="projects__item shimmer"
+			data-project-index="${index}"
+		>
+			<picture>
+				<source type="image/webp" srcset="${webp}">
+				<source type="image/jpeg" srcset="${jpeg}">
+				<img
+					src="${jpeg}"
+					alt="${title}"
+					fetchpriority="high"
+					onload="${handleImageLoad}"
+				>
+			</picture>
 			<div class="projects__item-title">
 				<svg><use href="#icon-eye" /></svg>
 				<h5>${title}</h5>
