6ea599b5a5646b1aef2ec8b2e2b6e10220192225
ASTIK <52823963+astik-dev@users.noreply.github.com>
2026-05-03 16:37:41 +0200

    feat: add AVIF source and responsive images to '.projects__item picture'

--- src/js/modules/renderProjectsItem.js
+++ src/js/modules/renderProjectsItem.js
@@ -1,14 +1,33 @@
-import imageCreator from "./imageCreator.js";
+const IMG_SIZES = [ 400, 600, 800, 1000, 1200, 1400 ];
+
+const sizesAttrValue =
+	"(max-width: 575.5px) min(400px, calc(100vw - 40px))," +
+	"(max-width: 767.5px) calc((100vw - (20px + 20px + 15px)) / 2)," +
+	"(max-width: 860px) calc((100vw - (20px + 20px + 20px)) / 2)," +
+	"400px";
+
+const handleImageLoad =
+	"this.closest('.projects__item').classList.remove('projects__item_shimmer')";
+
+function buildImgUrl(projectId, size, ext) {
+	if (!IMG_SIZES.includes(size)) {
+		throw new Error(
+			`Invalid size "${size}". Allowed sizes: ${IMG_SIZES.join(", ")}`
+		);
+	}
+	return (
+		"https://astik-dev.github.io/portfolio-images/projects/" +
+		`${projectId}/thumbnail/${size}.${ext}`
+	);
+}
+
+function buildImgSrcset(projectId, ext) {
+	return IMG_SIZES
+		.map(size => `${buildImgUrl(projectId, size, ext)} ${size}w`)
+		.join();
+};
 
 export function renderProjectsItem({ folder, title }, index, isHidden) {
-	
-	const img800 = "projects/" + folder + "/800";
-	const webp = imageCreator.fullPath("external", img800 + ".webp");
-	const jpeg = imageCreator.fullPath("external", img800 + ".jpeg");
-
-	const handleImageLoad =
-		"this.closest('.projects__item').classList.remove('projects__item_shimmer')";
-
 	return `
 		<article
 			class="
@@ -19,11 +38,21 @@ export function renderProjectsItem({ folder, title }, index, isHidden) {
 			data-project-index="${index}"
 		>
 			<picture>
-				<source type="image/webp" srcset="${webp}">
-				<source type="image/jpeg" srcset="${jpeg}">
+				<source
+					type="image/avif"
+					srcset="${buildImgSrcset(folder, "avif")}"
+					sizes="${sizesAttrValue}"
+				>
+				<source
+					type="image/webp"
+					srcset="${buildImgSrcset(folder, "webp")}"
+					sizes="${sizesAttrValue}"
+				>
 				<img
-					src="${jpeg}"
-					alt="${title}"
+					srcset="${buildImgSrcset(folder, "jpg")}"
+					sizes="${sizesAttrValue}"
+					src="${buildImgUrl(folder, 1000, "jpg")}"
+					alt="${title} thumbnail"
 					fetchpriority="high"
 					onload="${handleImageLoad}"
 				>
